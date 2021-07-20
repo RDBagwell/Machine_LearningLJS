444977e75d4f18ca1af196d1f9b6d00068d5a445
Robert B <robertb@lifesciencepublishers.com>
2021-07-20 17:13:49 -0600

    ML Updates

--- plinko/score.js
+++ plinko/score.js
@@ -5,11 +5,11 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const testsetSize = 50;
-  const [testSet, trainingSet] = splitDataSet(outputs, testsetSize);
-  _.range(1, 10).forEach(k => {
+  const testsetSize = 100;
+  const [testSet, trainingSet] = splitDataSet(minMax(outputs, 3), testsetSize);
+  _.range(1, 20).forEach(k => {
     const accuracy = _.chain(testSet)
-      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
       .size()
       .divide(testsetSize)
       .value();
@@ -19,7 +19,12 @@ function runAnalysis() {
 
 function knn(data, point, k) {
   return _.chain(data)
-    .map(row=> [distance(row[0], point), row[3]])
+    .map(row => {
+      return [
+        distance(_.initial(row), point), 
+        _.last(row)
+      ]
+    })
     .sortBy(row=> row[0])
     .slice(0, k)
     .countBy(row=>row[1])
@@ -32,7 +37,13 @@ function knn(data, point, k) {
   }
 
 function distance(pointA, pointB){
-  return Math.abs(pointA - pointB)
+  return (
+    _.chain(pointA)
+    .zip(pointB)
+    .map(([a,b])=> (a-b) **2)
+    .sum()
+    .value() ** 0.5
+    );
 }
 
 function splitDataSet(data, testCount) {
@@ -40,4 +51,22 @@ function splitDataSet(data, testCount) {
   const testSet= _.slice(shuffled, 0, testCount);
   const trainingSet = _.slice(shuffled, testCount);
   return [testSet, trainingSet];
+}
+
+
+function minMax(data, featurCount) {
+
+  const clonedData =_.cloneDeep(data);
+  for (let i = 0; i < featurCount; i++) {
+    const column = clonedData.map(row => row[i]);
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min)
+    }
+
+  }
+
+  return clonedData
 }
