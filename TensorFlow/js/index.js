eea1678a3ae532972169ba45d5d515451aa3b2a6
Robert B <robertb@lifesciencepublishers.com>
2021-09-01 15:24:27 -0600

    add files

--- TensorFlow/js/index.js
+++ TensorFlow/js/index.js
@@ -1,4 +1,29 @@
-const xs = tf.tensor([1, 2, 3, 4]);
-const ys = tf.tensor([5, 6, 7, 8]);
-ys.sub(xs).print();
+const features = tf.tensor([
+    [-121, 47],
+    [-121.2, 46.5],
+    [-122, 46.5],
+    [-120.9, 46.7]
+]);
 
+const labels = tf.tensor([
+    [200],
+    [250],
+    [215],
+    [240]
+]);
+
+const predictonPoint = tf.tensor([-121, 47]);
+
+const k = 2
+
+features
+.sub(predictonPoint)
+.pow(2)
+.sum(1)
+.pow(.5)
+.expandDims(1)
+.concat(labels, 1)
+.unstack()
+.sort((a, b) => a.get(0) > b.get(0) ? 1 : -1)
+.slice(0,k)
+.reduce((acc, pair) => acc + pair.get(1), 0 ) / k
